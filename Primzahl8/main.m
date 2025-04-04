27c3bc64ebab1c7df1495c583f94428665171056
Dr. Andreas Plagens <andreas@plagens.eu>
2025-04-04 15:02:00 +0200

    Initial Commit

---
+++ Primzahl8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Primzahl8
+//
+//  Created by Dr. Andreas Plagens on 04.04.25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
