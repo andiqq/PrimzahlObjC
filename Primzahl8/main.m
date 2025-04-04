f5c088f96567f940210a9d7360cc329cba762fee
Dr. Andreas Plagens <andreas@plagens.eu>
2025-04-04 20:27:26 +0200

    first commit

--- Primzahl8/main.m
+++ Primzahl8/main.m
@@ -6,11 +6,43 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <math.h>
+#import <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+int main(void) {
+    int maxNumberOfPrimes = 10000000;
+    int *primes = malloc(maxNumberOfPrimes * sizeof(int));
+    primes[0] = 2;
+    int candidate = 3;
+    int primesCount = 1;
+
+    NSDate *startTime = [NSDate date];
+
+    while (primesCount < maxNumberOfPrimes) {
+        int maxTeiler = (int)sqrt(candidate);
+        BOOL isPrime = YES;
+
+        for (int i = 0; i < primesCount; i++) {
+            if (primes[i] > maxTeiler) break;
+            if (candidate % primes[i] == 0) {
+                isPrime = NO;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primes[primesCount] = candidate;
+            primesCount++;
+        }
+
+        candidate += 2;
     }
+
+    NSTimeInterval executionTime = -[startTime timeIntervalSinceNow];
+
+    printf("Execution Time: %f seconds\n", executionTime);
+    printf("Number of primes: %d\n", primesCount);
+
+    free(primes); // Free allocated memory
     return 0;
 }
